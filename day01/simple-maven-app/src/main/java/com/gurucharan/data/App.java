9868fee1a723ab71995e6c7b1afff7bd112c1882
sfjbs <guru@gmail.com>
2023-09-01 06:58:10 +0000

    first simple app

---
+++ day01/simple-maven-app/src/main/java/com/gurucharan/data/App.java
@@ -0,0 +1,13 @@
+package com.gurucharan.data;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello I am Gurucharan!" );
+    }
+}
